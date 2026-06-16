9cdcbb37823f89ab21774511a8ab0e65f95fc5d0
juanhmp <lualeomen@gmail.com>
2026-06-16 08:51:25 -0300

    commit

--- Aula7/exercicio8.cpp
+++ Aula7/exercicio8.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 using namespace std;
-int lugares[6][8] = {{0, 0, 0, 0, 0, 0, 0, 0},
+int lugares[6][8] = {
                         {0, 0, 0, 0, 0, 0, 0, 0},
                         {0, 0, 0, 0, 0, 0, 0, 0},
                         {0, 0, 0, 0, 0, 0, 0, 0},
                         {0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0},};
+                        {0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0},
+                    };
+void VerificarAssento(int i, int j){
+    if(lugares[i][j] == 0){
+        cout << "O assento " << i+1 << "-" << j+1 << " esta livre." << endl;
+    } else {
+        cout << "O assento " << i+1 << "-" << j+1 << " esta ocupado." << endl;
+    }
+}
+
 int main(){
+    void VerificarAssento(int i, int j);
     int opcao = 0;
 for(int i=0; i<6; i++){
     for(int j=0; j<8; j++){
         cout << "Digite 3 para consultar o assento " << i+1 << "-" << j+1 << " ou 1 para reservar ou 2 para cancelar: " << endl;
         cin >> opcao;
         if(opcao == 3){
-            ConsultarAssento(i, j);
+            VerificarAssento(i, j);
         } else if(opcao == 1){
             if(lugares[i][j] == 0){
                 lugares[i][j] = 1;
@@ -29,11 +40,4 @@ for(int i=0; i<6; i++){
             }
     }
 }
-}
-void ConsultarAssento(int i, int j){
-    if(lugares[i][j] == 0){
-        cout << "O assento " << i+1 << "-" << j+1 << " esta disponivel." << endl;
-    } else {
-        cout << "O assento " << i+1 << "-" << j+1 << " esta reservado." << endl;
-    }
 }
